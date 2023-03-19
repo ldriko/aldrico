212753b154957ef395cade8f5d61bf863ca718ea
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-03-20 02:41:42 +0700

    Remove picture

--- src/pages/MainSection.vue
+++ src/pages/MainSection.vue
@@ -4,25 +4,21 @@ import TextSection from '../components/TextSection.vue'
 
 <template>
   <text-section>
-    <p>
-      <img class="portrait"
-           src="/heaven.webp"
-           srcSet="/heaven.webp 1x, /heaven.webp 2x, /heaven.webp 3x, /heaven.webp 4x"
-           alt="Heaven"
-           width="176"
-           height="233"
-      />
-    </p>
     <h1>Heaven Aldrico</h1>
     <p>
-      Hi&comma; I&rsquo;m Heaven. Loves to code with various languages, learn new technologies and
-      implement them in my daily use as long as they are necessary. Been a gamer my whole life and won’t be retiring
-      anytime soon.
+      Hi&comma; I&rsquo;m Heaven. Loves to code with various languages, learn
+      new technologies and implement them in my daily use as long as they are
+      necessary. Been a gamer my whole life and won’t be retiring anytime soon.
     </p>
-    <p>I also try a new habit of expressing my thoughts about anything through my writing.</p>
+    <p>
+      I also try a new habit of expressing my thoughts about anything through my
+      writing.
+    </p>
     <p>
       Currently studying Informatics Engineering at
-      <a href="https://www.upnjatim.ac.id/" target="_blank">UPN &ldquo;Veteran&rdquo; Jawa Timur</a>.
+      <a href="https://www.upnjatim.ac.id/" target="_blank"
+        >UPN &ldquo;Veteran&rdquo; Jawa Timur</a
+      >.
     </p>
   </text-section>
 </template>
