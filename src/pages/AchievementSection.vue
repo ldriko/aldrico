100a894dca01fddcb73ef0f6509447cc1e7353aa
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-03-21 19:26:15 +0700

    Update achivements

--- src/pages/AchievementSection.vue
+++ src/pages/AchievementSection.vue
@@ -6,16 +6,28 @@ import TextSection from '../components/TextSection.vue'
   <text-section subtitle="Achievements">
     <ul>
       <li>
-        <a target="_blank" href="/certificates/madig.jpeg">1st place Digital Magazine</a>
+        <a target="_blank" href="/certificates/lks-province.jpeg">
+          1st place VCP
+        </a>
+        by UPN &OpenCurlyDoubleQuote;Veteran&CloseCurlyDoubleQuote; Jawa Timur
+      </li>
+      <li>
+        <a target="_blank" href="/certificates/lks-province.jpeg">
+          3rd place LKS Provinsi
+        </a>
+        by the Ministry of Education
+      </li>
+      <li>
+        <a target="_blank" href="/certificates/lks-wilker.pdf">
+          1st place LKS Wilker
+        </a>
+        by the Ministry of Education
+      </li>
+      <li>
+        <a target="_blank" href="/certificates/madig.jpeg">
+          1st place Digital Magazine
+        </a>
         by Mapion IT
-      </li>
-      <li>
-        <a target="_blank" href="/certificates/lks-wilker.pdf">1st place LKS Wilker</a>
-        by the Ministry of Education
-      </li>
-      <li>
-        <a target="_blank" href="/certificates/lks-province.jpeg">3rd place LKS Provinsi</a>
-        by the Ministry of Education
       </li>
     </ul>
   </text-section>
